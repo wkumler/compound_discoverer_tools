66a39de89c2bcd083b6f6994a554419e919f2d8f
William Kumler <wkumler@uw.edu>
2026-01-02 15:12:21 -0800

    Converting BMIS script to standalone

--- bmis_scripting_node/bmis_node_script.R
+++ bmis_scripting_node/bmis_node_script.R
@@ -4,18 +4,18 @@ message("Script starting!")
 library(rjson)
 CD_json_in <- fromJSON(file=commandArgs()[6])
 
-node_dev_dir <- "C:/Users/Ingalls Lab/Desktop/compound_discoverer_tools"
-project_dir <- "/bmis_scripting_node"
-
-v <- lapply(CD_json_in$Tables, function(table_info_i){
-  print(table_info_i$TableName)
-  table_i <- read.table(table_info_i$DataFile, header=TRUE, check.names = FALSE)
-  name_i <- paste0(node_dev_dir, project_dir, "/", table_info_i$TableName, ".csv")
-  assign(x = gsub(" ", "_", table_info_i$TableName), table_i, envir = globalenv())
-  # write.csv(table_i, name_i, row.names = FALSE)
-})
-
-save.image(file=paste0(node_dev_dir, project_dir, "/node_envir.RData"))
+# node_dev_dir <- "C:/Users/Ingalls Lab/Desktop/compound_discoverer_tools"
+# project_dir <- "/bmis_scripting_node"
+# 
+# v <- lapply(CD_json_in$Tables, function(table_info_i){
+#   print(table_info_i$TableName)
+#   table_i <- read.table(table_info_i$DataFile, header=TRUE, check.names = FALSE)
+#   name_i <- paste0(node_dev_dir, project_dir, "/", table_info_i$TableName, ".csv")
+#   assign(x = gsub(" ", "_", table_info_i$TableName), table_i, envir = globalenv())
+#   # write.csv(table_i, name_i, row.names = FALSE)
+# })
+# 
+# save.image(file=paste0(node_dev_dir, project_dir, "/node_envir.RData"))
 
 # load(paste0(node_dev_dir, project_dir, "/node_envir.RData"))
 
@@ -55,11 +55,17 @@ Compounds_long <- Compounds %>%
   ) %>%
   filter(`File Name`!="Max")
 
-internal_standard_regex <- ", \\d"
-pooled_sample_regex <- "_Poo_"
-half_v_full_regex <- "Half|Full"
-min_improvement <- 0.2
-already_good <- 0.1
+internal_standard_regex <- CD_json_in$NodeParameters$`Internal standard regex`
+pooled_sample_regex <- CD_json_in$NodeParameters$`Pooled sample regex`
+half_v_full_regex <- CD_json_in$NodeParameters$`Dilution regex`
+min_improvement <- CD_json_in$NodeParameters$`Minimal improvement threshold`
+already_good <- CD_json_in$NodeParameters$`Already good enough threshold`
+
+# internal_standard_regex <- ", \\d"
+# pooled_sample_regex <- "_Poo_"
+# half_v_full_regex <- "Half|Full"
+# min_improvement <- 0.2
+# already_good <- 0.1
 
 all_IS <- Compounds_long %>%
   filter(str_detect(Name, internal_standard_regex)) %>%
